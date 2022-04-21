36dc269d57872934265ecf37bf0bc7a0703d2055
DAscencioSanchez <diaspm77@gmail.com>
2022-04-21 12:27:43 -0500

    Change to test spiderman class

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,6 +1,14 @@
-describe("Test suite Dummy description", () =>{
-    test('Case 1 dummy', () => {
-        const resultOfSomething = 1 + 2 
-        expect(resultOfSomething.toBe(18))
+describe("Unit Test for Spiderman class", () =>{
+    test('1) Create an spiderman object', () => {
+        // Aqui escribimos el codigo que queremos usar 
+        //Queremos instanciar un objeto spiderman con esta informacion
+        const andrewGarfield = new Spiderman("Spiderman Sony", 31, "Andrew Garfield",2, "Sony")
+
+        //Validamos la clase con esta informacion
+        expect(andrewGarfield.name).toBe("Spiderman Sony")
+        expect(andrewGarfield.age).toBe(31)
+        expect(andrewGarfield.actor).toBe("Andrew Garfield")
+        expect(andrewGarfield.movies).toBe(31)
+        expect(andrewGarfield.studio).toBe("Sony")
     });
 })
